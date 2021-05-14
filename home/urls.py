d3561d5ae65a4b8584f3af00d18f81bb99f772ff
rps228 <rps228@gmail.com>
2021-05-14 00:52:58 -0300

    edit`

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .import views 
 
 urlpatterns =[
-		path('',views.index,name='index')
+		path('',views.index,name='index'),
+		path('submit',views.submit,name='submit'),
+		path('main',views.main,name='main'),
+		path('logout_user',views.logout_user,name='logout_user'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+
+@login_required(login_url='/')
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/')
+def main(request):
+    return render(request,"main.html")
+
 
 def index(request):
     return render(request,"index.html")
+
+@csrf_protect
+def submit(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')       
+        print(username)
+        print(password)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/main')
+        else:
+            messages.error(request, 'Usuario ou senha invalido!!')
+    return redirect('/')
